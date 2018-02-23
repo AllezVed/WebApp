142fe8dc03509029d0a90a14763ecf01b415f07e
Harsh <harsh_202me@yahoo.com>
2018-02-23 17:28:50 -0500

    split locationID+property to location ID and property for all files

--- FlowMeterCheck.py
+++ FlowMeterCheck.py
@@ -6,16 +6,32 @@
 import pandas as pd
 import logging
 import os
-try:
-    os.remove("./Documents/Atlanta/FlowMeterGraphing/Test.csv")
-except OSError:
-    pass
+# try:
+#     os.remove("./Documents/Atlanta/FlowMeterGraphing/Test.csv")
+# except OSError:
+    # pass
 col_names = ['SiteID', 'Timestamp','Reading','Unit']
+filename = "INC-17.csv"
+filename = filename.replace(".csv","") #testing for filename replacement
 df= pd.read_csv('//423207-amviis1.mwhexternal.local/coatlanta/_output/INC-17.csv')
 df2 = pd.read_csv('//423207-amviis1.mwhexternal.local/coatlanta/_output/Utoy Railroad.csv')
 # df_test = pd.read_csv('./Documents/Atlanta/FlowMeterGraphing/Test.csv')
 df.head()
 df2.shape
+
+# def return_col(x):
+#     locid = 'na'
+#     param = 'na'
+    
+#     if filename in x:
+#         locid = filename
+
+#     if x.SiteID.str.split(filename)[1] in ('Level', 'Rainfall', 'Velocity'):    
+        
+#         param = x.SiteID.str.split(filename)[1]
+
+#     return (locid,param)
+
 
 
 # In[65]:
@@ -25,7 +41,10 @@ df2.drop_duplicates
 df.columns = col_names
 df2.columns = col_names
 df2 = df2[df2['Reading'] != 0 ]
-
+# print(df.SiteID.str.split(filename)
+df['LocationID'] = filename
+# print(df.SiteID.str.split(filename)[0][1])
+df['Property'] = df.SiteID.str.split(filename)[0][1]
 
 # In[66]:
 
@@ -34,8 +53,8 @@ df2 = df2[df2['Reading'] != 0 ]
 
 # In[67]:
 
-df.to_csv('./Documents/Atlanta/FlowMeterGraphing/Test.csv')
-df2.to_csv('./Documents/Atlanta/FlowMeterGraphing/Test.csv', mode = 'a', header = False)
+df.to_csv('./TestRuns/Test.csv')
+df2.to_csv('./TestRuns/Test.csv', mode = 'a', header = False)
 
 
 # In[ ]:
--- file_cleaner.py
+++ file_cleaner.py
@@ -14,7 +14,10 @@ path = '//423207-amviis1.mwhexternal.local/coatlanta/_output/'
 def is_non_zero_file(fpath):  
     return os.path.isfile(fpath) and os.path.getsize(fpath) > 0
 
-for filename in os.listdir(path): 
+# filenames = ['1371 Kimberly Way -SSO.csv', 'INC-17.csv']
+for filename in os.listdir(path):
+        # if filename not in ('111.csv', '1371 Kimberly Way -SSO.csv'):
+        #     break
         print(filename)
         if not is_non_zero_file(os.path.join(path,filename)):
             continue
@@ -37,13 +40,28 @@ for filename in os.listdir(path):
         # df.columns = col_names
         # df2.columns = col_names
         # df = df[df['Reading'] != 0 ]
-
+        def col_splitter(x):
+            loc_id = 'na'
+            param = 'na'
+            location = filename.replace('.csv','')
+            if location in x:
+                loc_id = location
+            if (location in x) and (len(x.split(location)[1]) != 0):
+                param = x.split(location)[1]
+            
+            return loc_id, param
+            
+        # df['Property'] = df['SiteID'].str.split(filename.replace(".csv",""),1)
+#         filename_check = filename.replace(".csv", "")
+#         df['LocationID'] = filename_check
+# # print(df.SiteID.str.split(filename)[0][1])
+#         df['Property'] = df.SiteID.str.split(filename_check)[0][1]
 
         # # In[66]:I
 
         # # df[df['Reading']!=0]
 
-
+        df['LocationID'] , df['Property'] = zip(*df['SiteID'].apply(col_splitter))
         # # In[67]:
 
         df.to_csv('Test.csv', mode = 'a', index = True, header = False)
